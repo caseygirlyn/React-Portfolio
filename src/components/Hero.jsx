89382caebffdb62d6264c1941bb5aae610eb7558
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-02 20:49:57 +0000

    add shadow to h2 tag

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
     return <div className="hero-banner mb-4 py-4 w-100">
         <div className="container d-md-flex align-items-md-end h-100 justify-content-md-between p-3">
             <img src="/images/photo.jpg" alt="Girlyn" className="rounded-circle avatar shadow" />
-                <h2 className="float-md-end bg-primary-color text-white mt-3 p-3 text-center">
+                <h2 className="float-md-end bg-primary-color text-white mt-3 p-3 text-center shadow">
                     Web Developer / SEO / GTM
                 </h2>
         </div>
