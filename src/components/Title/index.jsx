59340fbf58f2f46226ee7a4cd40185c39fa605c2
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-02 22:01:58 +0000

    delete unused components

--- src/components/Title/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Title(props) {
-  return <><h2 className="primary-color w-auto bg-white d-flex align-items-center mb-2">{props.children}</h2><div className="col-12"></div></>;
-}
--- src/components/Wrapper/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Wrapper(props) {
-  return <div className="wrapper container card-group"><section className="row p-3 border-1 mx-4 my-5">{props.children}</section></div>;
-}
