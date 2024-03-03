3e9af8744a18494c14a6f5f77513af682ae80164
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-03 11:11:58 +0000

    test createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap-icons/font/bootstrap-icons.css"
 import * as bootstrap from 'bootstrap'
 import './main.css'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Project from "./components/Project";
+import Projects from "./components/Projects";
+import projects from "./projects.json";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    },
+    {
+        path: 'projects',
+        element: [
+            <Header />,
+            <Hero />,
+            <Projects>
+                {projects.map((projectObj) => (
+                    <Project
+                        id={projectObj.id}
+                        key={projectObj.id}
+                        name={projectObj.name}
+                        image={projectObj.image}
+                        tech={projectObj.tech}
+                        url={projectObj.url}
+                        githubRepo={projectObj.githubRepo}
+                    />
+                ))
+                }
+            </Projects>,
+            <Footer />
+        ]
+    },
+    {
+        path: 'contact',
+        element: [<Header />, <Hero />, <Contact />, <Footer />]
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
-
