749023d32d50d776c5cf838d15a52d83d2503513
Girlyn Antillon <casey.girlyn@gmail.com>
2024-03-04 19:40:11 +0000

    import and use NavLink

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 export default function Project(props) {
     return <div className="cardContainer col-md-4 col-sm-12 w-lg-auto p-md-3 p-1 pb-3" id={props.id}>
@@ -6,12 +7,11 @@ export default function Project(props) {
             <img src={props.image} title={props.name} className="w-100" />
             <div className="card-body caption bg-primary-color text-white py-1">
                 <p className="card-title fw-bolder mb-0">{props.name}</p>
-                <p className="card-text mb-0 fst-italic">
-                    {props.tech}
-                </p>
+                <p className="card-text mb-0 fst-italic">{props.tech}</p>
                 <small>
-                    <a href={props.url} target="_blank" rel="noreferrer" className="card-link text-white">Live Link</a>
-                    { props.githubRepo ? <a href={props.githubRepo} target="_blank" rel="noreferrer" className="card-link text-white">GitHub Repo</a> : ''}
+                <NavLink to={props.slug} rel="noreferrer" className="card-link text-white">View Project</NavLink>
+                    <NavLink to={props.url} target="_blank" rel="noreferrer" className="card-link text-white">Live Link</NavLink>
+                    { props.githubRepo ? <NavLink to={props.githubRepo} target="_blank" rel="noreferrer" className="card-link text-white">GitHub Repo</NavLink> : ''}
                 </small>
             </div>
         </div>
