b3decd7ae16acda36b39d69af4eaf47aacba614f
Girlyn Antillon <casey.girlyn@gmail.com>
2024-07-03 11:30:17 +0100

    fix image with low resolution

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Hero() {
     return <div className="hero-banner mb-4 py-4 w-100">
         <div className="container d-md-flex align-items-md-end h-100 justify-content-md-between p-3">
-            <img src="/images/photo.jpg" alt="Girlyn" className="rounded-circle avatar shadow" width={160} height={156}/>
+            <img src="/images/photo.jpg" alt="Girlyn" className="rounded-circle avatar shadow" width={160} height={160}/>
                 <h2 className="float-md-end bg-primary-color text-white mt-3 p-3 text-center shadow">
                     Full-Stack Developer / SEO / GTM
                 </h2>
