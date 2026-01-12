065d7892a50b14605c8ce3f562223f159ad6bc86
Girlyn Antillon <casey.girlyn@gmail.com>
2026-01-12 11:06:38 +0000

    remove instagram link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,6 @@ export default function Footer() {
         <div className="col-md-12 ps-md-5 column-gap-md-5 d-flex flex-wrap fs-6 justify-content-center">
             <a href="https://github.com/caseygirlyn" target="_blank" className="btn"><i className="bi bi-github"></i> github</a>
             <a href="https://www.linkedin.com/in/girlyncasey/" target="_blank" className="btn"><i className="bi bi-linkedin"></i> linkedin</a>
-            <a href="https://www.instagram.com/gicasey/" target="_blank" className="btn"><i className="bi bi-instagram"></i> instagram</a>
         </div>
     </footer>
 }
