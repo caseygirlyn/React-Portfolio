faaf8e41d9036f994e7ce2cc25e4467aced3e6c8
Girlyn Antillon <casey.girlyn@gmail.com>
2026-03-18 10:37:27 +0000

    update projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 export default function Project(props) {
     return <div className="cardContainer col-md-4 col-sm-12 w-lg-auto p-md-3 p-1 pb-3" id={props.id}>
         <div className="card border-2 rounded-0 w-auto shadow">
-            <img src={props.image} title={props.name} className="w-100" />
+            <img src={props.image} title={props.name} className="w-100"/>
             <div className="card-body caption bg-primary-color text-white py-1">
                 <p className="card-title fw-bolder mb-0">{props.name}</p>
                 <p className="card-text mb-0 fst-italic">{props.tech}</p>
